f44c7a38e04b53700c1a4c3d4debb90cb595572b
ink404 <kirkpatrickian6@gmail.com>
2018-08-19 10:27:23 -0500

    initial commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,30 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess The number!");
+
+    let secret_num = rand::thread_rng().gen_range(1, 100);
+    // println!("The secret num is: {}", secret_num);
+
+    println!("Input a number: ");
+    // variables immutable by default - have to delcare mut
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("failed to readline!");
+    println!("You guessed {}",guess);
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Guess too small!"),
+        Ordering::Equal => println!("WINNER!"),
+        Ordering::Greater => println!("Guess too big!"),
+    }
+
+    println!("Secret was {}",secret_num);
+}
